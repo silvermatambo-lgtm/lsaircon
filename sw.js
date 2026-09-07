855a28ab4074b1e56464746cd50637e16b8d1067
webdevproproject <silvermatambo@gmail.com>
2026-09-07 13:03:33 +0200

    Refresh service worker for SVG ETA logo

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "lsaircon-v7-final-logo";
-const CORE = ["/", "/about.html", "/services.html", "/gallery.html", "/contact.html", "/manifest.webmanifest", "/images/eta-logo-final.jpg", "/images/eta-app-icon.svg", "/assets/style.css", "/assets/redesign.css", "/assets/identity.css"];
+const CACHE = "lsaircon-v8-svg-logo";
+const CORE = ["/", "/about.html", "/services.html", "/gallery.html", "/contact.html", "/manifest.webmanifest", "/images/eta-web-logo.svg", "/images/eta-app-icon.svg", "/assets/style.css", "/assets/redesign.css", "/assets/identity.css"];
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(CORE)).then(() => self.skipWaiting()));
 });
